f60caabb9c201180655c60415e60be580a096af4
Muhammad Junaedi <49475719+MJunJunJun@users.noreply.github.com>
2021-11-16 10:03:10 +0700

    Initial commit

---
+++ Assets/APIHelper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Net;
+using System.IO;
+
+public static class APIHelper 
+{
+    public static Cek getNewCek()
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.chucknorris.io/jokes/random");
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string json = reader.ReadToEnd();
+        return JsonUtility.FromJson<Cek>(json);
+
+
+    }
+}
---
+++ Assets/cekDanlatihan.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class cekDanlatihan : MonoBehaviour
+{
+    public void Start()
+    {
+        textCek.text = PlayerPrefs.GetString("value");
+    }
+    public TextMeshProUGUI textCek;
+    public void cekFunction()
+    {
+        Cek cek = APIHelper.getNewCek();
+        textCek.text = cek.value;
+        PlayerPrefs.SetString("value",cek.value);
+    }
+}
